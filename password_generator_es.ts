fc99527b6cf112dd05220c9d76467858ce9bf26d
Dr. Eric O. Flores <eric@florestoro.com>
2024-09-16 16:44:54 -0600

    Spanish Translation .ts file
    This is the Spanish Translation .ts file.

---
+++ password_generator_es.ts
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>PasswordGenerator</name>
+    <message>
+        <location filename="password_generator.cpp" line="78"/>
+        <source>File</source>
+        <translation type="unfinished">Archivo</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="79"/>
+        <source>Quit</source>
+        <translation type="unfinished">Salir</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="102"/>
+        <source>Help</source>
+        <translation type="unfinished">Ayuda</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="103"/>
+        <source>About</source>
+        <translatorcomment>Acerca de esta aplicación</translatorcomment>
+        <translation type="unfinished">Acerca de esta Aplicacion</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="82"/>
+        <source>Settings</source>
+        <translation type="unfinished">Ajustes</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="83"/>
+        <source>Toggle Dark Mode</source>
+        <translation type="unfinished">Activar o desactivar el modo oscuro</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="84"/>
+        <source>Language</source>
+        <translation type="unfinished">Lenguaje</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="109"/>
+        <source>Enter your password length (4 to 256):</source>
+        <translation type="unfinished">Ingrese su contraseña Longitud (4 a 256)</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="117"/>
+        <source>Custom character set:</source>
+        <translation type="unfinished">Conjunto de caracteres personalizados</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="123"/>
+        <source>Upper Case Only</source>
+        <translation type="unfinished">Solo mayúsculas</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="124"/>
+        <source>Lower Case Only</source>
+        <translation type="unfinished">Solo minúsculas</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="125"/>
+        <source>Combination of Upper and Lower Case</source>
+        <translation type="unfinished">Combinación de mayúsculas y minúsculas</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="126"/>
+        <source>Use Special Characters</source>
+        <translation type="unfinished">Utilice caracteres especiales</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="127"/>
+        <source>Use Numbers</source>
+        <translation type="unfinished">Utilice números</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="129"/>
+        <source>Require at least one uppercase letter</source>
+        <translation type="unfinished">Requiere al menos una letra mayúscula</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="130"/>
+        <source>Require at least one lowercase letter</source>
+        <translation type="unfinished">Requiere al menos una letra minúscula</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="131"/>
+        <source>Require at least one number</source>
+        <translation type="unfinished">Requiere al menos un número</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="132"/>
+        <source>Require at least one special character</source>
+        <translation type="unfinished">Requiere al menos un carácter especial</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="133"/>
+        <source>Exclude similar characters (0, O, 1, l)</source>
+        <translation type="unfinished">Excluir caracteres similares</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="147"/>
+        <source>Password Complexity:</source>
+        <translation type="unfinished">Complejidad de la contraseña</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="156"/>
+        <source>Password Strength:</source>
+        <translation type="unfinished">Fuerza de la contraseña</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="165"/>
+        <source>Generate Password</source>
+        <translation type="unfinished">Generar contraseña</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="166"/>
+        <source>Clear</source>
+        <translation type="unfinished">Limpiar Resultados</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="167"/>
+        <source>Copy to Clipboard</source>
+        <translation type="unfinished">Copiar al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="187"/>
+        <location filename="password_generator.cpp" line="343"/>
+        <source>Password Generator</source>
+        <translation type="unfinished">Generador de contraseñas</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="218"/>
+        <source>Error</source>
+        <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="218"/>
+        <source>Please select at least one character set!</source>
+        <translation type="unfinished">¡Seleccione al menos un conjunto de caracteres!</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="245"/>
+        <source>Weak</source>
+        <translation type="unfinished">débil</translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="247"/>
+        <source>Moderate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="249"/>
+        <source>Strong</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="270"/>
+        <source>Input Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="270"/>
+        <source>Please enter a valid length between 4 and 256.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="286"/>
+        <source>Copied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="286"/>
+        <source>Password copied to clipboard!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="291"/>
+        <source>About Password Generator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="password_generator.cpp" line="292"/>
+        <source>Password Generator - Version 1.5
+Author: Dr. Eric O. Flores
+(c) Copyrights 2024 Dr. Eric O. Flores
+
+This software uses C++ with Qt for the GUI.
+Language Packages Enhancements coded by ChatGPT.
+Licensed under GPL 3.
+
+This software is free to use and distribute.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
