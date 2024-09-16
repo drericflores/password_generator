10241c8f06c7dfd0645f19e699d5737837da8269
Dr. Eric O. Flores <eric@florestoro.com>
2024-09-16 17:33:56 -0600

    Update password_generator.cpp

--- password_generator.cpp
+++ password_generator.cpp
@@ -4,12 +4,12 @@
  * Copyright 2024, Dr. Eric O. Flores <eoftoro@gmail.com>
  * 
  *
- * Title of Software is free software: you can redistribute it and/or modify
+ * Password Generator is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Title of Software is distributed in the hope that it will be useful,
+ * Password Generator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
