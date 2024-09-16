82ccf058388cf57b9dc74a656b70d0513b5506f6
Dr. Eric O. Flores <eric@florestoro.com>
2024-09-16 17:33:19 -0600

    Update password_generator.cpp

--- password_generator.cpp
+++ password_generator.cpp
@@ -1,3 +1,24 @@
+/*
+ * Password Generator
+ * 
+ * Copyright 2024, Dr. Eric O. Flores <eoftoro@gmail.com>
+ * 
+ *
+ * Title of Software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Title of Software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Title of Software.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <QApplication>
 #include <QWidget>
 #include <QVBoxLayout>
